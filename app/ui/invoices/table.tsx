64ce5fce31cdad1d67fa4f6c4e6dd2bf2a80f00e
DeShawn Humphries <deshawn.humphries@gmail.com>
2023-11-28 01:16:06 -0500

    Added "alt" to the Image component

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="Table file in the dashboard project"
                       />
                       <p>{invoice.name}</p>
                     </div>
