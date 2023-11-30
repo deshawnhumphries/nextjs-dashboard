cea51f0cf7da91c01d104e0b4d4fdcf4549b564d
DeShawn Humphries <deshawn.humphries@gmail.com>
2023-11-30 01:49:17 -0500

    Added data fetch and fixed card components

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,32 +15,23 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
-      <div>
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
-        <Card
-          title="Collected"
-          value={/*totalPaidInvoices*/ 0}
-          type="collected"
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
         />
-        <Card
-          title="Pending"
-          value={/*totalPendingInvoices*/ 0}
-          type="pending"
-        />
-        <Card
-          title="Total Invoices"
-          value={/*numberOfInvoices*/ 0}
-          type="invoices"
-        />
-        <Card
-          title="Total Customers"
-          value={/*numberOfCustomers*/ 0}
-          type="customers"
-        />
-      </div>
     </>
   );
 }
